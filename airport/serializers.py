f8bf29527354ae34328027fce3db392deafe7941
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-23 15:48:27 +0200

    feat: implement MealOptionSerializer in airport app

---
+++ airport/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from airport.models import MealOption
+
+
+class MealOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MealOption
+        fields = ("name", "meal_type", "weight", "price")
