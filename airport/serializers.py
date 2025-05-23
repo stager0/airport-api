affff9992b89ca6c6e57af87425ae39d602c6af1
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-23 15:57:54 +0200

    feat: implement CrewSerializer for airport app

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from airport.models import MealOption, SnacksAndDrinks, ExtraEntertainmentAndComfort, Airport
+from airport.models import MealOption, SnacksAndDrinks, ExtraEntertainmentAndComfort, Airport, Crew
 
 
 class MealOptionSerializer(serializers.ModelSerializer):
@@ -25,3 +25,9 @@ class AirportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airport
         fields = ("name", "closest_big_city")
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("first_name", "last_name", "position")
