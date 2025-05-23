129bcc4808700f88aac8f865de58514b0091ff21
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-23 15:50:44 +0200

    feat: implement SnacksAndDrinksSerializer for airport app

--- airport/serializers.py
+++ airport/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from airport.models import MealOption
+from airport.models import MealOption, SnacksAndDrinks
 
 
 class MealOptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MealOption
         fields = ("name", "meal_type", "weight", "price")
+
+
+class SnacksAndDrinksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SnacksAndDrinks
+        fields = ("name", "price")
