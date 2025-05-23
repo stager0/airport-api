741bbe2451281ee06c586d6efb1d6cdcb8b1d49e
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-23 17:00:02 +0200

    feat: implement SnaksAndDrinksViewSet for airport app

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airport.models import MealOption, SnacksAndDrinks
+from airport.serializers import (
+    MealOptionSerializer,
+    SnacksAndDrinksSerializer,
+)
+
+
+class MealOptionViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = MealOption.objects.all()
+    serializer_class = MealOptionSerializer
+
+
+class SnacksAndDrinksViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = SnacksAndDrinks.objects.all()
+    serializer_class = SnacksAndDrinksSerializer
