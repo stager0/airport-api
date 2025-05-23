aefde3bfa11094dd7e0605281ba3ba9d7f461692
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-23 17:54:54 +0200

    feat: add URL's for airport module

--- airport/urls.py
+++ airport/urls.py
@@ -1,5 +1,32 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    MealOptionViewSet,
+    SnacksAndDrinksViewSet,
+    ExtraEntertainmentAndComfortViewSet,
+    AirportViewSet,
+    CrewViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    RouteViewSet,
+    FlightViewSet,
+    OrderViewSet
+)
 
 app_name = "airport"
 
-urlpatterns=[]
+router = routers.DefaultRouter()
+
+router.register("meal_option", MealOptionViewSet)
+router.register("snacks_and_drinks", SnacksAndDrinksViewSet)
+router.register("extra_entertainment_and_comfort", ExtraEntertainmentAndComfortViewSet)
+router.register("airport", AirportViewSet)
+router.register("crew", CrewViewSet)
+router.register("airplane_type", AirplaneTypeViewSet)
+router.register("airplane", AirplaneViewSet)
+router.register("route", RouteViewSet)
+router.register("flight", FlightViewSet)
+router.register("order", OrderViewSet)
+
+urlpatterns=[path("", include(router.urls))]
