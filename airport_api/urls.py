c48ec3d0f890201bfa9c27fd8e7a9f03144cd57a
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-26 17:25:52 +0200

    docs: add api/doc, api/doc/swagger, api/doc/redoc endpoints

--- airport_api/urls.py
+++ airport_api/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/airport/", include("airport.urls", namespace="airport")),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger_ui"),
+    path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
 ]
