4232bbbb054db18b3b6e78b12527f9e9ef629883
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-26 21:24:23 +0200

    docs: implement swagger documentation (list, create) for AirplaneTypeViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,8 +1,5 @@
-from http.client import responses
-
 from django.db.models import Count, F, Q
 from django.db.models.functions import Length
-from django.db.transaction import mark_for_rollback_on_error
 from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiParameter, OpenApiResponse, OpenApiExample
 from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -475,10 +472,61 @@ class CrewViewSet(
         return queryset.distinct()
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get a list of all Airplane Types",
+        description="Retrieve a list of all Airplane Types (owned by the company)",
+        tags=["airplane_type"],
+        request=AirplaneTypeSerializer,
+        responses={
+            200: AirplaneTypeSerializer(many=True),
+            401: OpenApiResponse(description="Authentication credentials were not provided")
+        },
+        examples=[
+            OpenApiExample(
+                "Item from AirplaneType List Example",
+                value={
+                    "id": 1,
+                    "name": "Boeing 777"
+                },
+            )
+        ]
+    ),
+    create=extend_schema(
+        summary="Create an Airplane Type",
+        description="Create an Airplane Type with provided name",
+        tags=["airplane_type"],
+        request=AirplaneType,
+        responses={
+            201: AirplaneType,
+            401: OpenApiResponse(description="Authentication credentials were not provided"),
+            400: OpenApiResponse(
+                description="Bad Request",
+                examples=[
+                    OpenApiExample(
+                        "Empty Airplane Name",
+                        value={
+                                "name": ["This field may not be blank."]
+                        },
+                        status_codes=[400]
+                    )
+                ]
+            )
+        },
+        examples=[
+            OpenApiExample(
+                "Create new Airplane Type",
+                value={
+                    "name": "Boeing",
+                },
+                request_only=True
+            )
+        ]
+    ),
+)
 class AirplaneTypeViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
     GenericViewSet
 ):
     queryset = AirplaneType.objects.all()
