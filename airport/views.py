133073d1aa620a1aa8bac2b5d18bb45699354768
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-26 21:07:51 +0200

    docs: implement swagger documentation (list, create) for CrewViewSet

--- airport/views.py
+++ airport/views.py
@@ -379,7 +379,82 @@ class AirportViewSet(
         return queryset.distinct()
 
 
-class CrewViewSet(viewsets.ModelViewSet):
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get a list of all Crews(workers)",
+        description="Retrieve a list of all actual Crews in format (first_name, last_name, position)",
+        tags=["crew"],
+        request=CrewSerializer,
+        responses={
+            200: CrewSerializer(many=True),
+            401: OpenApiResponse(description="Authentication credentials were not provided")
+        },
+        parameters=[
+            OpenApiParameter(
+                name="first_name", description="Filter by first_name",
+                required=False, type=str
+            ),
+            OpenApiParameter(
+                name="last_name", description="Filter by last_name",
+                required=False, type=str
+            ),
+            OpenApiParameter(
+                name="position", description="Filter by position",
+                required=False, type=str
+            )
+        ],
+        examples=[
+            OpenApiExample(
+                "Item from Crew List Example",
+                value={
+                    "id": 1,
+                    "first_name": "James",
+                    "last_name": "Kirk",
+                    "position": "CAPTAIN"
+                },
+            )
+        ]
+    ),
+    create=extend_schema(
+        summary="Create a Crew Person",
+        description="Create a crew Person with provided data (first_name, last_name, position)",
+        tags=["crew"],
+        request=CrewSerializer,
+        responses={
+            201: CrewSerializer,
+            401: OpenApiResponse(description="Authentication credentials were not provided"),
+            400: OpenApiResponse(
+                description="Bad Request",
+                examples=[
+                    OpenApiExample(
+                        "Empty first_name and last_name",
+                        value={
+                            "first_name": ["This field may not be blank."],
+                            "last_name": ["This field may not be blank."]
+                        },
+                        status_codes=[400]
+                    )
+                ]
+            )
+        },
+        examples=[
+            OpenApiExample(
+                "Create new Crew Person",
+                value={
+                    "first_name": "John",
+                    "last_name": "Muller",
+                    "position": "CAPTAIN"
+                },
+                request_only=True
+            )
+        ]
+    )
+)
+class CrewViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+    ):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = [IsAdminOrIsAuthenticatedReadOnly]
