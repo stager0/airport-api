a2bdc9c5a87c272eca3b45de39e8bcc0a6a7dff2
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-26 20:56:13 +0200

    docs: implement swagger documentation (list, create) for AirportViewSet

--- airport/views.py
+++ airport/views.py
@@ -127,6 +127,7 @@ class MealOptionViewSet(
 
         return queryset.distinct()
 
+
 @extend_schema_view(
     list=extend_schema(
         summary="Get a list of all Snacks and Drinks",
@@ -218,6 +219,12 @@ class SnacksAndDrinksViewSet(
         description="Retrieve a list with all Extra Entertainment and Comfort items",
         tags=["extra_entertainment_and_comfort"],
         request=ExtraEntertainmentAndComfortSerializer,
+        parameters=[
+            OpenApiParameter(
+                name="name", description="Filter by item name (tablet, pillow etc.)",
+                required=False, type=str
+            )
+        ],
         responses={
             200: ExtraEntertainmentAndComfortSerializer(many=True),
             401: OpenApiResponse(description="Authentication credentials were not provided")
@@ -226,7 +233,7 @@ class SnacksAndDrinksViewSet(
             OpenApiExample(
                 "List Example",
                 value=[
-                    {"id": 1,"name": "Tablet with movies/games","price": "9.99"},
+                    {"id": 1, "name": "Tablet with movies/games", "price": "9.99"},
                 ],
                 response_only=True
             )
@@ -260,7 +267,8 @@ class SnacksAndDrinksViewSet(
                 value={
                     "name": "Tablet",
                     "price": 17.50
-                }
+                },
+                request_only=True
             )
         ]
     )
@@ -284,7 +292,76 @@ class ExtraEntertainmentAndComfortViewSet(
         return queryset.distinct()
 
 
-class AirportViewSet(viewsets.ModelViewSet):
+@extend_schema_view(
+    list=extend_schema(
+        summary="Get a list of Airports",
+        description="Retrieve a list of all available Airports (from which this company flies)",
+        tags=["airport"],
+        request=AirportSerializer,
+        responses={
+            200: AirportSerializer(many=True),
+            401: OpenApiResponse(description="Authentication credentials were not provided")
+        },
+        parameters=[
+            OpenApiParameter(
+                name="closest_big_city", description="Filter by closest_big_city",
+                required=False, type=str
+            ),
+            OpenApiParameter(
+                name="airport_name", description="Filter by airport_name",
+                required=False, type=str
+            )
+        ],
+        examples=[
+            OpenApiExample(
+                "List Example",
+                value={
+                    "id": 1,
+                    "name": "JFK International Airport",
+                    "closest_big_city": "New York City"
+                }
+            )
+        ]
+    ),
+    create=extend_schema(
+        summary="Create an Airport",
+        description="Create an Airport with provided data (name (airport name), closest big city)",
+        tags=["airport"],
+        request=AirportSerializer,
+        responses={
+            201: AirportSerializer,
+            401: OpenApiResponse(description="Authentication credentials were not provided"),
+            400: OpenApiResponse(
+                description="Bad Request",
+                examples=[
+                    OpenApiExample(
+                        "Empty name and closest_big_city",
+                        value={
+                            "name": ["This field may not be blank."],
+                            "closest_big_city": ["This field may not be blank."]
+                        },
+                        status_codes=[400]
+                    )
+                ]
+            )
+        },
+        examples=[
+            OpenApiExample(
+                "Create new Airport",
+                value={
+                    "name": "Airport Boryspil, Kiev",
+                    "closest_big_city": "Kiev"
+                },
+                request_only=True
+            )
+        ]
+    )
+)
+class AirportViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
     permission_classes = [IsAdminOrIsAuthenticatedReadOnly]
