e3f19b378d8669013b57e99b663b2011becc9016
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-27 19:59:25 +0200

    feat: implement get_queryset (with query optimization) and get_crew (crew is M2M)

--- airport/admin.py
+++ airport/admin.py
@@ -29,7 +29,7 @@ admin.site.register(DiscountCoupon)
 class FlightAdmin(admin.ModelAdmin):
     list_display = (
         "id", "route", "airplane",
-        "departure_time", "arrival_time", "crew",
+        "departure_time", "arrival_time", "get_crew",
         "price_economy", "price_business", "luggage_price_1_kg"
     )
     list_filter = (
@@ -48,6 +48,15 @@ class FlightAdmin(admin.ModelAdmin):
         "price_economy"
         )
     ordering = ("-departure_time",)
+
+    @admin.display(description="Crew")
+    def get_crew(self, obj):
+        return " ,".join(f"{person.first_name} {person.last_name}, {person.position}" for person in obj.crew.all())
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related(
+            "route__source", "route__destination", "airplane",
+        ).prefetch_related("crew")
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -69,8 +78,8 @@ class TicketAdmin(admin.ModelAdmin):
         "order",
         "price",
         "meal_option",
-        "extra_entertainment_and_comfort",
-        "snacks_and_drinks",
+        "get_extra_entertainment_and_comfort",
+        "get_snacks_and_drinks",
         "is_business",
         "discount_coupon"
     )
@@ -85,3 +94,9 @@ class TicketAdmin(admin.ModelAdmin):
     search_fields = ("discount_coupon__code", "discount")
     ordering = ("-order__created_at",)
     readonly_fields = ("discount",)
+
+    def get_extra_entertainment_and_comfort(self, obj):
+        return " ,".join([f"{extra.name} ({extra.price}$)" for extra in obj.get_extra_entertainment_and_comfort.all()])
+
+    def get_snacks_and_drinks(self, obj):
+        return " ,".join([f"{snack.name} ({snack.price}$)" for snack in obj.snacks_and_drinks.all()])
