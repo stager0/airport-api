5fb2a6c5035aaacca78f839afdea49af477a9394
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-27 17:13:24 +0200

    docs: move ExtraEntertainmentAndComfort documentation to separate extra_entertainment_and_comfort_schema file

---
+++ airport/schema/extra_entertainment_and_comfort_schema.py
@@ -0,0 +1,65 @@
+from drf_spectacular.utils import OpenApiExample, OpenApiResponse, extend_schema, OpenApiParameter, extend_schema_view
+
+from airport.serializers import ExtraEntertainmentAndComfortSerializer
+
+
+extra_entertainment_and_comfort = extend_schema_view(
+    list=extend_schema(
+        summary="Get all from Extra Entertainment and Comfort",
+        description="Retrieve a list with all Extra Entertainment and Comfort items",
+        tags=["extra_entertainment_and_comfort"],
+        request=ExtraEntertainmentAndComfortSerializer,
+        parameters=[
+            OpenApiParameter(
+                name="name", description="Filter by item name (tablet, pillow etc.)",
+                required=False, type=str
+            )
+        ],
+        responses={
+            200: ExtraEntertainmentAndComfortSerializer(many=True),
+            401: OpenApiResponse(description="Authentication credentials were not provided")
+        },
+        examples=[
+            OpenApiExample(
+                "List Example",
+                value=[
+                    {"id": 1, "name": "Tablet with movies/games", "price": "9.99"},
+                ],
+                response_only=True
+            )
+        ]
+    ),
+    create=extend_schema(
+        summary="Create a new Extra Entertainment and Comfort item",
+        description="Create a new item Extra Entertainment and Comfort by provided data (name, price)",
+        request=ExtraEntertainmentAndComfortSerializer,
+        tags=["extra_entertainment_and_comfort"],
+        responses={
+            201: ExtraEntertainmentAndComfortSerializer,
+            401: OpenApiResponse(description="Authentication credentials were not provided"),
+            400: OpenApiResponse(
+                description="Bad request",
+                examples=[
+                    OpenApiExample(
+                        "Empty (incorrect) Name and Price",
+                        value={
+                            "name": ["This field may not be blank."],
+                            "price": ["A valid number is required."]
+                        },
+                        status_codes=["400"]
+                    ),
+                ]
+            )
+        },
+        examples=[
+            OpenApiExample(
+                "Create Example",
+                value={
+                    "name": "Tablet",
+                    "price": 17.50
+                },
+                request_only=True
+            )
+        ]
+    )
+)
--- airport/views.py
+++ airport/views.py
@@ -22,6 +22,7 @@ from airport.models import (
     Order, DiscountCoupon
 )
 from airport.permissions import IsAdminOrIsAuthenticatedReadOnly
+from airport.schema.extra_entertainment_and_comfort_schema import extra_entertainment_and_comfort
 from airport.schema.meal_option_schema import meal_option_schema
 from airport.schema.snacks_and_drinks_schema import snacks_and_drinks_schema
 from airport.serializers import (
@@ -85,66 +86,7 @@ class SnacksAndDrinksViewSet(
         return queryset.distinct()
 
 
-@extend_schema_view(
-    list=extend_schema(
-        summary="Get all from Extra Entertainment and Comfort",
-        description="Retrieve a list with all Extra Entertainment and Comfort items",
-        tags=["extra_entertainment_and_comfort"],
-        request=ExtraEntertainmentAndComfortSerializer,
-        parameters=[
-            OpenApiParameter(
-                name="name", description="Filter by item name (tablet, pillow etc.)",
-                required=False, type=str
-            )
-        ],
-        responses={
-            200: ExtraEntertainmentAndComfortSerializer(many=True),
-            401: OpenApiResponse(description="Authentication credentials were not provided")
-        },
-        examples=[
-            OpenApiExample(
-                "List Example",
-                value=[
-                    {"id": 1, "name": "Tablet with movies/games", "price": "9.99"},
-                ],
-                response_only=True
-            )
-        ]
-    ),
-    create=extend_schema(
-        summary="Create a new Extra Entertainment and Comfort item",
-        description="Create a new item Extra Entertainment and Comfort by provided data (name, price)",
-        request=ExtraEntertainmentAndComfortSerializer,
-        tags=["extra_entertainment_and_comfort"],
-        responses={
-            201: ExtraEntertainmentAndComfortSerializer,
-            401: OpenApiResponse(description="Authentication credentials were not provided"),
-            400: OpenApiResponse(
-                description="Bad request",
-                examples=[
-                    OpenApiExample(
-                        "Empty (incorrect) Name and Price",
-                        value={
-                            "name": ["This field may not be blank."],
-                            "price": ["A valid number is required."]
-                        },
-                        status_codes=["400"]
-                    ),
-                ]
-            )
-        },
-        examples=[
-            OpenApiExample(
-                "Create Example",
-                value={
-                    "name": "Tablet",
-                    "price": 17.50
-                },
-                request_only=True
-            )
-        ]
-    )
-)
+@extra_entertainment_and_comfort
 class ExtraEntertainmentAndComfortViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
