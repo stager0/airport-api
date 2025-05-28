ffd5a2c86e9f14a545ee56da08bbd31462ef29ec
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-28 18:10:48 +0200

    feat(user): implement admin page without username field

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import UserChangeForm, AdminUserCreationForm, AdminPasswordChangeForm
+from django.utils.translation import gettext_lazy as _
+
+from user.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    add_form_template = "admin/auth/user/add_form.html"
+    change_user_password_template = None
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
+    form = UserChangeForm
+    add_form = AdminUserCreationForm
+    change_password_form = AdminPasswordChangeForm
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
