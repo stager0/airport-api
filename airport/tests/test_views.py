c353fdca74941fe3084728ab73b598169e285132
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-28 22:01:14 +0200

    tests: implement test_airport_list_status_200_and_contains_value (AirportApiTests)

--- airport/tests/test_views.py
+++ airport/tests/test_views.py
@@ -1,18 +1,21 @@
-from http.client import responses
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from airport.models import AirplaneType, Airplane
+from airport.models import (
+    AirplaneType,
+    Airplane,
+    Airport
+)
 
 
 class BaseCase(TestCase):
     def setUp(self):
         self.airplane_type = AirplaneType.objects.create(name="Passage")
         self.airplane = Airplane.objects.create(name="Boeing", rows=10, letters_in_row="ABCDEFGH", airplane_type=self.airplane_type)
+        self.airport = Airport.objects.create(name="International Airport Odessa", closest_big_city="Odessa")
         self.user = get_user_model().objects.create_user(
             email="test_email@test.com",
             first_name="Vasyl",
@@ -38,7 +41,7 @@ class AirplaneTypeApiTests(BaseCase):
         super().setUp()
         self.list_url = reverse("airport:airplanetype-list")
 
-    def test_airplane_type_list(self):
+    def test_airplane_type_list_status_200_and_contains_value(self):
         response = self.client.get(self.list_url)
 
         self.assertEqual(response.status_code, 200)
@@ -60,7 +63,7 @@ class AirplaneApiTests(BaseCase):
         super().setUp()
         self.list_url = reverse("airport:airplane-list")
 
-    def test_airplane_list_200(self):
+    def test_airplane_list_status_200_and_contains_value(self):
         response = self.client.get(self.list_url)
 
         self.assertEqual(response.status_code, 200)
@@ -86,9 +89,20 @@ class AirplaneApiTests(BaseCase):
 
         self.assertEqual(response.status_code, 201)
 
-    def test_properties_capacity_list_of_seats_seats_in_row_count(self):
+    def test_properties_capacity_list_of_seats_seats_in_row_count_return_without_mistakes(self):
         airplane = self.airplane
 
         self.assertEqual(airplane.capacity, 80)
         self.assertEqual(airplane.list_of_seats, ["A", "B", "C", "D", "E", "F", "G", "H"])
         self.assertEqual(airplane.seats_in_row_count, 8)
+
+class AirportApiTests(BaseCase):
+    def setUp(self):
+        super().setUp()
+        self.list_url = reverse("airport:airport-list")
+
+    def test_airport_list_status_200_and_contains_value(self):
+        response = self.client.get(self.list_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Odessa")
