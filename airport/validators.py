d40f26905f12ee91fdc9586b09ba0bcb93cb9a0a
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-29 21:17:18 +0200

    feat(validator): implement custom validator for validation valid_until field in DiscountCoupon

--- airport/validators.py
+++ airport/validators.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from django.utils import timezone
+from django.utils.timezone import make_aware
 from rest_framework.exceptions import ValidationError
 
 
@@ -6,3 +10,9 @@ def validate_discount_coupon_code(value: str) -> ValidationError | None:
         raise ValidationError(f"The code: '{value}' must be 10 symbols long")
     if not value.isalnum():
         raise ValidationError(f"The code: '{value}' must contain only letters or numbers")
+
+def validate_discount_date(date: datetime) -> ValidationError | None:
+    if date < timezone.now() :
+        raise ValidationError(f"The date (valid until) must be later than {timezone.now}")
+    if date > make_aware(datetime(2099, 12, 12)):
+        raise ValidationError(f"The date must be earlier then {date}")
